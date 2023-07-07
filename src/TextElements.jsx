43bda280f70af0ef305bd4fa5b87db68f2747af1
Hayden Walker <haydenwalker@live.com>
2023-07-07 10:33:10 -0300

    Cleaned up code

--- src/TextElements.jsx
+++ src/TextElements.jsx
@@ -5,27 +5,25 @@ import './App.css';
 /**
  * Page header.
  */
-export function Header() {
-    const title = "wzlog";
-    //const desc = "Amateur radio logbook by VE9WZ"
+export function Header() {  
+  return(
+    <Box id="header-box">
+      <Typography variant="h1">wzlog</Typography>
+      <Typography variant="body2"><i>Bare-bones ham radio logbook written in React by <a href="https://www.haywalk.ca">VE9WZ</a></i></Typography>
+    </Box>
+  );
+}
   
-    return(
-      <Box id="header-box">
-        <Typography variant="h1">{title}</Typography>
-      </Box>
-    );
-  }
-  
-  /**
-   * Horizontal line with text. 
-   */
+/**
+ * Horizontal line with text. 
+ */
 export function HorizontalLine({text}) {
-    return(
-      <div style={{marginBottom: '15px'}}>
-        <Divider textAlign='left' >
-          <Typography variant="body1">{text}</Typography>
-        </Divider>
-      </div>
-    );
-  }
+  return(
+    <div style={{marginBottom: '15px'}}>
+      <Divider textAlign='left' >
+        <Typography variant="body1">{text}</Typography>
+      </Divider>
+    </div>
+  );
+}
   
